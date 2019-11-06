06156d99e20d4ccd356ffc41d844c8f2b040bb67
Rodrigues Junior <marcos-roberto.rodrigues-junior@dxc.com>
2019-11-06 00:11:33 -0300

    database sql

---
+++ database.sql
@@ -0,0 +1,50 @@
+CREATE USER postgres WITH PASSWORD 'admin';
+
+-- Database: dsdproj
+
+-- DROP DATABASE dsdproj;
+
+CREATE DATABASE dsdproj
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+-- Table: public.tb_user
+
+-- DROP TABLE public.tb_user;
+
+CREATE TABLE public.tb_user
+(
+    surname character varying(30) COLLATE pg_catalog."default",
+    nickname character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(45) COLLATE pg_catalog."default" NOT NULL,
+    status character(1) COLLATE pg_catalog."default",
+    id integer NOT NULL DEFAULT nextval('tb_user_id_seq'::regclass),
+    CONSTRAINT tb_user_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.tb_user
+    OWNER to postgres;
+
+-- Table: public.tb_group
+
+-- DROP TABLE public.tb_group;
+
+CREATE TABLE public.tb_group
+(
+    name character varying(40) COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL DEFAULT nextval('tb_group_id_seq'::regclass),
+    CONSTRAINT tb_group_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.tb_group
+    OWNER to postgres;
